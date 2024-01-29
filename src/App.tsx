15446f63148fb73912cd78f42548dd5c8ffe6bb1
jatin <ym.jatin@gmail.com>
2024-01-29 23:42:58 +0530

    chore: fetch data using custom hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
+import { Mainmenu } from '@/components'
+import { useGetForecastByCoord, useGetWeatherByCity } from '@/hooks'
+import { useEffect, useMemo, useState } from 'react'
+
 export const App = () => {
-    return <div className="bg-background p-2 w-screen h-screen"></div>
+    const [ city, _setCity ] = useState( 'pune' )
+    const { data: currentWeatherData } = useGetWeatherByCity( 'pune' )
+    const coord = useMemo( () => currentWeatherData.coord, [ currentWeatherData ] )
+    const { data: forecastData } = useGetForecastByCoord( coord )
+
+    return (
+        <div className="bg-background p-2 w-screen h-screen flex flex-col gap-4">
+            <Mainmenu />
+            <div className="bg-slate-300 w-full max-h-32 h-1/5 rounded-3xl"></div>
+        </div>
+    )
 }
