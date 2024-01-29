fd0f4ab111dc6264191ac5742219137839ffd3f3
jatin <ym.jatin@gmail.com>
2024-01-29 23:39:34 +0530

    chore: add types for current and forecast weather data

---
+++ src/types/types.d.ts
@@ -0,0 +1,99 @@
+export interface IWeatherData {
+    coord: Coord
+    weather: Weather[]
+    base: string
+    main: Main
+    visibility: number
+    wind: Wind
+    clouds: Clouds
+    dt: number
+    sys: Sys
+    timezone: number
+    id: number
+    name: string
+    cod: number
+}
+
+export interface Clouds {
+    all: number
+}
+
+export interface Coord {
+    lon: number
+    lat: number
+}
+
+export interface Main {
+    temp: number
+    feels_like: number
+    temp_min: number
+    temp_max: number
+    pressure: number
+    humidity: number
+    sea_level: number
+    grnd_level: number
+}
+
+export interface Sys {
+    country: string
+    sunrise: number
+    sunset: number
+}
+
+export interface Weather {
+    id: number
+    main: string
+    description: string
+    icon: string
+}
+
+export interface Wind {
+    speed: number
+    deg: number
+    gust: number
+}
+
+// ---
+
+export interface IForeCastData {
+    cod: string
+    message: number
+    cnt: number
+    list: List[]
+    city: City
+}
+
+export interface City {
+    id: number
+    name: string
+    coord: Coord
+    country: string
+    population: number
+    timezone: number
+    sunrise: number
+    sunset: number
+}
+
+export interface List {
+    dt: number
+    main: MainClass
+    weather: Weather[]
+    clouds: Clouds
+    wind: Wind
+    visibility: number
+    pop: number
+    sys: Sys
+    dt_txt: Date
+}
+
+export interface MainClass {
+    temp: number
+    feels_like: number
+    temp_min: number
+    temp_max: number
+    pressure: number
+    sea_level: number
+    grnd_level: number
+    humidity: number
+    temp_kf: number
+}
