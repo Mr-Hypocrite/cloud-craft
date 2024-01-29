ecaad0ce4f3fa5f7f274320d7308de3e8e37d487
jatin <ym.jatin@gmail.com>
2024-01-29 22:37:28 +0530

    chore: add and configure react-query

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from '@/App'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './output.css'
+
+const queryClient = new QueryClient( {
+    defaultOptions: {
+        queries: {
+            suspense: true,
+            refetchOnWindowFocus: true,
+            staleTime: 5 * 1000,
+        },
+    },
+} )
 
 ReactDOM.createRoot( document.getElementById( 'root' )! ).render(
     <React.StrictMode>
-        <App />
+        <QueryClientProvider client={queryClient}>
+            <App />
+        </QueryClientProvider>
     </React.StrictMode>
 )
