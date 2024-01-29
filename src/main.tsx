5ba7721332e4411a96936de584f79905723fe43a
jatin <ym.jatin@gmail.com>
2024-01-29 23:42:03 +0530

    chore: add suspense wrapper around App comp

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from '@/App'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
@@ -17,7 +17,9 @@ const queryClient = new QueryClient( {
 ReactDOM.createRoot( document.getElementById( 'root' )! ).render(
     <React.StrictMode>
         <QueryClientProvider client={queryClient}>
-            <App />
+            <Suspense>
+                <App />
+            </Suspense>
         </QueryClientProvider>
     </React.StrictMode>
 )
