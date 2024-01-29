5a426cfd19a6ad92f7cdb7ec3523ed5e6af0617e
jatin <ym.jatin@gmail.com>
2024-01-29 23:41:27 +0530

    chore: add useGetForecastByCoord hook

--- src/hooks/weather/index.ts
+++ src/hooks/weather/index.ts
@@ -1,1 +1,2 @@
 export * from './useGetWeatherByCity'
+export * from './useGetForecastByCoord'
---
+++ src/hooks/weather/useGetForecastByCoord.tsx
@@ -0,0 +1,28 @@
+import { IForeCastData } from '@/types'
+import { useQuery } from '@tanstack/react-query'
+
+type GetWeatherForecastCoordParams = {
+    lat: number
+    lon: number
+}
+
+const getForecastByCoordFn = async ( { lat, lon }: GetWeatherForecastCoordParams ) => {
+    console.log( lat, lon )
+    if ( lat && lon ) {
+        const response = await fetch(
+            `${import.meta.env.VITE_OPEN_WEATHER_API_BASE_URL}forecast?lat=${lat}&lon=${lon}&appid=${
+                import.meta.env.VITE_OPEN_WEATHER_MAP_API_KEY
+            }`
+        )
+        const data = await response.json()
+        return data
+    }
+    return null
+}
+
+export const useGetForecastByCoord = ( { lat, lon }: GetWeatherForecastCoordParams ) => {
+    return useQuery<IForeCastData>( {
+        queryKey: [ 'forecast-by-city-lat-lon', lat, lon ],
+        queryFn: () => getForecastByCoordFn( { lat, lon } ),
+    } )
+}
